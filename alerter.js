6991203c9055c0d4b8d3c91dcde2881b5efaedca
vamshi <vamshi.paka@gmail.com>
2020-05-01 09:26:06 +0530

    jsfile

---
+++ alerter.js
@@ -0,0 +1,7 @@
+let fName=prompt("What is Your First name?");
+let lName=prompt("What is Your Last name?");
+let age=prompt("How old are you?");
+alert("Your fullname is " + fName +" "+ lName);
+alert("your age is " +age);
+console.log("Your fullname is "+ fName + " " + lName);
+console.log("you are " + age + " years old");
